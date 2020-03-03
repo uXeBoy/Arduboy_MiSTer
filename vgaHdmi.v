239838e6a12478c4be81475ce8c7d9a7037a4885
Dan O'Shea <6539132+uXeBoy@users.noreply.github.com>
2020-03-04 06:34:16 +1030

    Update video with 15KHz mode.

--- vgaHdmi.v
+++ vgaHdmi.v
@@ -29,7 +29,7 @@ module vgaHdmi
   output reg ce_pix,
   output reg hsync, vsync,
   output reg hblank, vblank,
-  output reg pixelValue
+  output     pixelValue
 );
 
 reg  [7:0] mem [1024];
@@ -58,8 +58,8 @@ always @ (posedge oled_clk or posedge reset) begin
     end
     else begin // commands
       if (shiftCount == 3'b111) begin
-			if(shiftLeft[7:1] == 'b1010011) invert <= shiftLeft[0]; // invert (A6/A7)
-			if(shiftLeft[7:3] == 'b10110) waddr <= {shiftLeft[2:0], 7'b000_0000}; //page 0-7 (B0-B7)
+            if(shiftLeft[7:1] == 'b1010011) invert <= shiftLeft[0]; // invert (A6/A7)
+            if(shiftLeft[7:3] == 'b10110) waddr <= {shiftLeft[2:0], 7'b000_0000}; //page 0-7 (B0-B7)
       end
       else shiftReg <= shiftLeft;
       shiftCount <= shiftCount + 1'b1;
@@ -71,54 +71,59 @@ wire [7:0] tempByte = mem[raddr];
 
 reg ce_pix_pre, ce_pix_int;
 always @ (posedge clock) begin
-	reg [2:0] div;
+    reg [3:0] div;
 
-	div <= div + 1'd1;
+    div <= div + 1'd1;
 
-	ce_pix_pre <= !div;
-	ce_pix_int <= !div[1:0] & div[2];
-	ce_pix     <= !div[1:0]; //twice higher pixel clock to fit OSD on VGA/DV
+    ce_pix_pre <= !div;
+    ce_pix_int <= !div[2:0] & div[3];
+    ce_pix     <= !div[2:0]; //twice higher pixel clock to fit OSD on VGA/DV
 end
+
+assign pixelValue = pixel & ~vblank_int;
+reg pixel, vblank_int;
 
 // Manejo de Pixeles y Sincronizacion
 always @ (posedge clock) begin
-	reg       invertLatched;
-	reg       old_ce;
-	reg [2:0] vdiv;
-	reg [7:0] pixelH, pixelV;
+    reg       invertLatched;
+    reg       old_ce;
+    reg       vdiv;
+    reg [7:0] pixelH, pixelV;
 
-	old_ce <= ce_pix_pre;
+    old_ce <= ce_pix_pre;
 
-	if (ce_pix_pre) begin
-		pixelH <= pixelH + 1'b1;
-		if(pixelH == 159) begin
-			pixelH <= 0;
-			vdiv <= vdiv + 1'd1;
-			if(vdiv[2]) begin
-				vdiv <= 0;
-				pixelV <= pixelV + 1'd1;
-				if(pixelV == 104) begin
-					invertLatched <= invert;
-					pixelV <= 0;
-				end
-			end
-		end
-	end
+    if (ce_pix_pre) begin
+        pixelH <= pixelH + 1'b1;
+        if(pixelH == 159) begin
+            pixelH <= 0;
+            vdiv <= ~vdiv;
+            if(vdiv) begin
+                pixelV <= pixelV + 1'd1;
+                if(pixelV == 130) begin
+                    invertLatched <= invert;
+                    pixelV <= 0;
+                end
+            end
+        end
+    end
 
-	if (old_ce) raddr <= {pixelV[5:3],pixelH[6:0]};
+    if (old_ce) raddr <= {pixelV[5:3],pixelH[6:0]};
 
-	if (ce_pix_int) begin
-		pixelValue <= invertLatched ^ tempByte[pixelV[2:0]];
+    if (ce_pix_int) begin
+        pixel <= invertLatched ^ tempByte[pixelV[2:0]];
 
-		if(pixelV == 0)   vblank <= 0;
-		if(pixelV == 64)  vblank <= 1;
-		if(pixelV == 82)  vsync  <= !vdiv[2];
+        if(pixelV == 127) vblank     <= 0;
+        if(pixelV == 0)   vblank_int <= 0;
+        if(pixelV == 64)  vblank_int <= 1;
+        if(pixelV == 68)  vblank     <= 1;
 
-		if(pixelH == 0)   hblank <= 0;
-		if(pixelH == 128) hblank <= 1;
-		if(pixelH == 132) hsync  <= 1;
-		if(pixelH == 147) hsync  <= 0;
-	end
+        vsync  <= (pixelV[7:1] == 47);
+
+        if(pixelH == 0)   hblank <= 0;
+        if(pixelH == 128) hblank <= 1;
+        if(pixelH == 135) hsync  <= 1;
+        if(pixelH == 150) hsync  <= 0;
+    end
 end
 
 endmodule
