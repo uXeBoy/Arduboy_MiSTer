a4f303b80be4335a272b600f04f1fb14d6f6519e
Dan O'Shea <6539132+uXeBoy@users.noreply.github.com>
2020-03-23 11:59:08 +1030

    New Relase - fix ram clearing / bus muxer

--- avr/mega-ram.v
+++ avr/mega-ram.v
@@ -21,60 +21,37 @@
 `timescale 1ns / 1ps
 
  module mega_ram  #(
-    parameter PLATFORM = "XILINX",
-    parameter MEM_MODE = "BLOCK",
     parameter ADDR_BUS_WIDTH = 12,  /* < in address lines */
     parameter DATA_BUS_WIDTH = 8,
     parameter RAM_PATH = ""
 ) (
-    input clk,
-    input cs,
-    input we,
-    input re,
-    input rst,
-    output reg halt,
-    input [ADDR_BUS_WIDTH-1:0] a,
-    input [DATA_BUS_WIDTH - 1:0] d_in,
-    output [DATA_BUS_WIDTH - 1:0] d_out
+    input      rst,
+    input      clk,
+    input      we,
+    input      [ADDR_BUS_WIDTH - 1:0] a,
+    input      [DATA_BUS_WIDTH - 1:0] d_in,
+    output reg [DATA_BUS_WIDTH - 1:0] d_out
 );
-
-generate
-
-if(PLATFORM == "XILINX")
-begin
 
 reg [DATA_BUS_WIDTH - 1:0] mem [(2**ADDR_BUS_WIDTH)-1:0];
 
 initial begin
 if (RAM_PATH != "")
-    $readmemh({RAM_PATH, ".mem"}, mem);
+     $readmemh({RAM_PATH, ".mem"}, mem);
 end
 
-integer clear_cnt;
 always @ (posedge clk) begin
-    if (rst) halt <= 1'b1;
-    if (halt) begin
-        for (clear_cnt = 0; clear_cnt < 256; clear_cnt = clear_cnt + 1)
-        begin : CLEAR_RAM
-            if (clear_cnt != 255) begin
-                mem[clear_cnt] <= 8'h00;
-            end
-            else if (clear_cnt == 255) begin
-                halt <= 1'b0;
-            end
-        end
+    reg [7:0] clear_cnt = 0;
+
+    if (rst) begin
+        mem[clear_cnt] <= 0;
+        clear_cnt <= clear_cnt + 1'd1;
     end
-    else if (cs & we & (a < 12'hB00)) mem[a] <= d_in;
+    else if (we) begin
+        mem[a] <= d_in;
+    end
 end
 
-reg [DATA_BUS_WIDTH - 1:0]d_out_tmp;
+always @ (posedge clk) d_out <= mem[a];
 
-always @ (posedge clk) begin
-    d_out_tmp <= mem[a];
-end
-assign d_out = (cs & re) ? d_out_tmp : 8'b00;
-
-end
-
-endgenerate
 endmodule
